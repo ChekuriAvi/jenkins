5cf4a9e57d4599756c93345e2ff2fdfe561f492d
ChekuriAvi <77680895+ChekuriAvi@users.noreply.github.com>
2021-03-03 13:39:27 -0500

    Update User.c

--- src/User.c
+++ src/User.c
@@ -9,7 +9,7 @@
 **                  Germany                      WWW    www.ipg-automotive.com
 ******************************************************************************
 **
-** F unctions
+** Functions
 ** ---------
 **
 ** Initialization
